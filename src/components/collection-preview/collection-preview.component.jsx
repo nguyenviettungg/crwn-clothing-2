e12d13b7732a275f8c228233ccdd6d123f2097aa
nguyenviettungg <nguyentung696969@gmail.com>
2020-10-01 23:01:17 +0700

    add collection page

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+            {items
+                .filter((item, index) => index < 4)
+                .map(({id,...itemProps}) => (
+          <CollectionItem key={id}{...itemProps}/>
+        ))}
+    </div>
+  </div>
+);
+export default CollectionPreview;
